5594fadc8c9772bf57b7020e02ef6228004e33e6
nicatsemedov777 <smdovn42@gmail.com>
2023-11-11 20:57:16 +0400

    first commit

---
+++ src/test/java/az/iktlab/learnlink/LearnLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package az.iktlab.learnlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
