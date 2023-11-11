24df609c7034acb9e0d28a7e862aaa9d57b597e9
Karim <2KarimKarimov004@gmail.com>
2023-11-11 22:30:45 +0400

    swagger configuration

---
+++ src/main/java/az/iktlab/learnlink/configuration/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package az.iktlab.learnlink.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("http://localhost:8080")))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer").bearerFormat("jti"))
+                                .addSecuritySchemes("basicAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("basic"))
+                )
+                .security(List.of(new SecurityRequirement().addList("basicAuth").addList("bearerAuth")))
+                .info(new Info().title("Book Store").version("V1"));
+    }
+}
